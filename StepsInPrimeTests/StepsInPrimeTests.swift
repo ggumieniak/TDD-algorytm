6f50180e9a4d2860a740c8bdf9acee975e0bf30a
Grzegorz Gumieniak <w65314@student.wsiz.edu.pl>
2022-04-16 22:42:18 +0200

    Add seventh testcase (validation more range of data)

--- StepsInPrimeTests/StepsInPrimeTests.swift
+++ StepsInPrimeTests/StepsInPrimeTests.swift
@@ -79,4 +79,23 @@ class StepsInPrimeTests: XCTestCase {
 		XCTAssertEqual(first, resultFirt)
 		XCTAssertEqual(second, resultSecond)
 		}
+	
+	func test_getPrimes_step8_from300to400() {
+		let (first,second) = (359,367)
+		guard let (resultFirt,resultSecond) = try? stepsInPrime.getPrimes(8, 300, 400) else { return XCTFail() }
+		XCTAssertEqual(first, resultFirt)
+		XCTAssertEqual(second, resultSecond)
+		}
+	
+	func test_getPrimes_step2_from2to12() {
+		let (first,second) = (3,5)
+		guard let (resultFirt,resultSecond) = try? stepsInPrime.getPrimes(2, 2, 100) else { return XCTFail() }
+		XCTAssertEqual(first, resultFirt)
+		XCTAssertEqual(second, resultSecond)
+		}
+	
+	func test_getPrimes_step2_from4900to4919() {
+		let result = try? stepsInPrime.getPrimes(2, 4900, 4919)
+		XCTAssertNil(result)
+		}
 }
